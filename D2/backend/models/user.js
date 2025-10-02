393ce4115305394bbf5b34d2d6d090522374afaf
Amadeus Fidos <u22526162@tuks.co.za>
2025-10-02 08:53:15 +0200

    created user model to make typescript addition easier

---
+++ D2/backend/models/user.js
@@ -0,0 +1,14 @@
+export default User = {
+    id: null,
+    username: '',
+    email: '',
+    password: '',
+    profile: {
+      name: '',
+      bio: '',
+      avatar: ''
+    },
+    friends: [],
+    createdAt: null,
+    updatedAt: null
+  };
